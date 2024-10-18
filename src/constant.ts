03feda85e53e8582ae14a310de41bc0a0af23896
Rishabh <108469021+Rishabh-raj-kumar@users.noreply.github.com>
2024-10-18 23:57:15 +0530

    first push

---
+++ src/constant.ts
@@ -0,0 +1,12 @@
+const SWATCHES = [
+    "#000000",  // black
+    "#ffffff",  // white
+    "#ee3333",  // red
+    "#e64980",  // pink
+    "#be4bdb",  // purple  // brown
+    "#228be6",  // blue  // dark blue
+    "#40c057",  // green  // dark green
+    "#fab005",  // yellow // orange
+];
+  
+export { SWATCHES };
